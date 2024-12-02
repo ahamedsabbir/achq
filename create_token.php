b869de020d6c7f2e6aa1eadd640e4ae4b74cc25e
ahamedsabbir <isratahanedsabbir@gmail.com>
2024-12-02 17:11:52 +0600

    update

--- create_token.php
+++ create_token.php
@@ -21,8 +21,7 @@ curl_setopt_array($curl, [
         "products" => ["auth"],
         "country_codes" => ["US"],
         "language" => "en",
-        "webhook" => "https://webhook.sample.com",
-        "redirect_uri" => "https://achq.reigeeky.com/make_public_token.html",
+        "webhook" => "https://achq.reigeeky.com/",
     ]),
 ]);
 
